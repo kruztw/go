ba2ad4ac04d949873428fd5dc1c2b2c34bda6caf
raybai <raybai@synology.com>
2022-08-16 11:20:53 +0800

    update simple.go

--- cgo/simple.go
+++ cgo/simple.go
@@ -2,19 +2,24 @@ package main
 
 // comment for golang
 
-// // comment for C
-// #include <stdio.h>
-// #include <stdlib.h>
-//
-// static void myprint(char* s) {
-//   printf("%s\n", s);
-// }
-// // no space between c code and import C !!
+/* // comment for C
+#include <stdio.h>
+#include <stdlib.h>
+
+static char *echo(char* s) {
+   printf("%s\n", s);
+   return s;
+}
+
+// no space between c code and import C !!
+*/
 import "C"
 import "unsafe"
+import "fmt"
 
 func main() {
 	cs := C.CString("Hello World")
-	C.myprint(cs)
+	res := C.echo(cs)
+	fmt.Printf("res = %v\n", C.GoString(res))
 	C.free(unsafe.Pointer(cs))
 }
