c3efe8db139cd7db55b8dd96cfe2056f294c671a
raybai <raybai@synology.com>
2022-10-03 18:10:18 +0800

    update database/sqlite3

--- database/sqlite3/simple.go
+++ database/sqlite3/simple.go
@@ -1,39 +1,46 @@
 package main
 
 import (
-    "fmt"
-    "database/sql"
-    _ "github.com/mattn/go-sqlite3"
+	"database/sql"
+	"fmt"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
 var db *sql.DB
 
 func main() {
-    var err error
-    db, err = sql.Open("sqlite3", "./sqlite.db")
+	var err error
+	db, err = sql.Open("sqlite3", "./sqlite.db")
 
-    stmt, _ := db.Prepare("CREATE TABLE IF NOT EXISTS users(username, password)")
-    stmt.Exec()
+	stmt, _ := db.Prepare("CREATE TABLE IF NOT EXISTS users(username, password)")
+	stmt.Exec()
 
-    stmt, _ = db.Prepare("INSERT INTO users(username, password) VALUES (?, ?)")
-    stmt.Exec("kruztw", "secret")
+	stmt, _ = db.Prepare("INSERT INTO users(username, password) VALUES (?, ?)")
+	stmt.Exec("kruztw", nil)
 
-    checkError(err)
-    query := "select * from users;"
-    rows, err := db.Query(query)
-    checkError(err)
+	checkError(err)
+	query := "select * from users;"
+	rows, err := db.Query(query)
+	checkError(err)
 
-    for rows.Next() {
-        var username, password string
-        err = rows.Scan(&username, &password)
-        checkError(err)
-        fmt.Printf("username = %s, password = %s\n", username, password)
-    }
+	var pw sql.NullString // avoid error "converting NULL to string is unsupported"
+	for rows.Next() {
+		var username, password string
+		err = rows.Scan(&username, &pw)
+		checkError(err)
+
+		if pw.Valid {
+			password = pw.String
+		}
+
+		fmt.Printf("username = %s, password = %s\n", username, password)
+	}
 }
 
 func checkError(err error) {
-    if err != nil {
-        fmt.Println(err.Error())
-        panic(err)
-    }
+	if err != nil {
+		fmt.Println(err.Error())
+		panic(err)
+	}
 }
