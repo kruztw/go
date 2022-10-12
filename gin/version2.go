8ac4be19b68d2b5858d44c126b4137752eb21f01
kruztw <kruztw@gmail.com>
2022-10-12 23:32:01 +0800

    update gin/

---
+++ gin/version2.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+type IndexData struct {
+	Title   string
+	Content string
+}
+
+type header struct {
+	Username string `header:"username"`
+	Password string `header:"password`
+}
+
+func test(c *gin.Context) {
+	var h header
+	if err := c.ShouldBindHeader(&h); err != nil {
+		c.String(404, "err: %v", err)
+		return
+	}
+
+	fmt.Printf("h = %v\n", h)
+
+	data := new(IndexData)
+	data.Title = "首頁"
+	data.Content = "我的第一個首頁"
+	c.HTML(http.StatusOK, "index.html", data)
+}
+
+func main() {
+	router := gin.Default()
+	router.LoadHTMLGlob("template/*")
+	router.GET("/", test)
+
+	srv := &http.Server{
+		Addr:    ":8080",
+		Handler: router,
+	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			fmt.Printf("listen failed: %v\n", err)
+			return
+		}
+	}()
+
+	quit := make(chan os.Signal)
+	signal.Notify(quit, os.Interrupt)
+
+	<-quit
+	fmt.Print("Shutdown Server ...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		fmt.Printf("Server Shutdown: %v", err)
+		return
+	}
+
+	fmt.Println("Server exiting")
+}
