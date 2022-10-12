4b8a91398500a03be4abaa2b2247322dad0330bc
kruztw <kruztw@gmail.com>
2022-10-12 23:10:39 +0800

    update gin/

--- gin/simple.go
+++ gin/simple.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -11,7 +12,20 @@ type IndexData struct {
 	Content string
 }
 
+type header struct {
+	Username string `header:"username"`
+	Password string `header:"password`
+}
+
 func test(c *gin.Context) {
+	var h header
+	if err := c.ShouldBindHeader(&h); err != nil {
+		c.String(404, "err: %v", err)
+		return
+	}
+
+	fmt.Printf("h = %v\n", h)
+
 	data := new(IndexData)
 	data.Title = "首頁"
 	data.Content = "我的第一個首頁"
