592d1579b3531c184dde6dc610e1135418452468
kruztw <kruztw@gmail.com>
2022-10-17 21:33:07 +0800

    update time/

--- time/simple.go
+++ time/simple.go
@@ -1,13 +1,19 @@
 package main
 
 import (
-    "fmt"
-    "time"
+	"fmt"
+	"time"
 )
 
+func foo() {
+	fmt.Printf("foo now: %v\n", time.Now())
+	time.AfterFunc(5*time.Second, foo)
+}
+
 func main() {
-    for range time.Tick(time.Second*2) {    // 每 2s 觸發一次  
-        time.Sleep(time.Second);            // 睡 1s
-        fmt.Println(time.Now());            // 照理講應該是每 3s 印一次, 但事實上是每 2s 印一次
-    }
+	go foo()
+	for range time.Tick(time.Second * 2) { // 每 2s 觸發一次
+		time.Sleep(time.Second) // 睡 1s
+		fmt.Println(time.Now()) // 照理講應該是每 3s 印一次, 但事實上是每 2s 印一次
+	}
 }
