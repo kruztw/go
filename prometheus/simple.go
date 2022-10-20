39c1d119e0ae297e0e1f723536acff0537f7e5a0
raybai <raybai@synology.com>
2022-10-20 10:07:24 +0800

    add prometheus/

---
+++ prometheus/simple.go
@@ -0,0 +1,35 @@
+// http://localhost:8888/metrics
+
+package main
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+func recordMetrics() {
+	go func() {
+		for {
+			opsProcessed.Inc()
+			time.Sleep(2 * time.Second)
+		}
+	}()
+}
+
+var (
+	opsProcessed = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "myapp_processed_ops_total",
+		Help: "The total number of processed events",
+	})
+)
+
+func main() {
+	recordMetrics()
+
+	http.Handle("/metrics", promhttp.Handler())
+	http.ListenAndServe("127.0.0.1:8888", nil)
+}
