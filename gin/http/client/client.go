fa1aed5483b23e0f0d728fb79a2b0233c3ca805a
raybai <raybai@synology.com>
2022-10-20 14:07:54 +0800

    update gin/

--- gin/http/client/client.go
+++ gin/http/client/client.go
@@ -2,25 +2,27 @@ package main
 
 import (
 	"bytes"
-	"crypto/tls"
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"net/url"
 )
 
-type RegisterRequest struct{}
+type RegisterRequest struct {
+	Msg string
+}
 
 func main() {
 
-	body := RegisterRequest{}
+	body := RegisterRequest{Msg: "hello world"}
 
 	bBody, err := json.Marshal(body)
 	if err != nil {
 		return
 	}
 
-	url := url.URL{Scheme: "https", Host: "127.0.0.1:8888", Path: "/"}
+	url := url.URL{Scheme: "http", Host: "127.0.0.1:8888", Path: "/post"}
 
 	req, err := http.NewRequest(
 		"POST",
@@ -37,10 +39,7 @@ func main() {
 		"Bearer ",
 	)
 
-	tr := &http.Transport{
-		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-	}
-	client := &http.Client{Transport: tr}
+	client := &http.Client{}
 
 	resp, err := client.Do(req)
 	if err != nil {
@@ -50,4 +49,6 @@ func main() {
 
 	defer resp.Body.Close()
 
+	bytes, err := ioutil.ReadAll(resp.Body)
+	fmt.Printf("resp.body = %v\n", string(bytes))
 }
--- gin/http/server/server.go
+++ gin/http/server/server.go
@@ -31,6 +31,21 @@ func get(c *gin.Context) {
 	c.HTML(http.StatusOK, "index.html", data)
 }
 
+func post(c *gin.Context) {
+	jsonData, err := c.GetRawData()
+	if err != nil {
+		fmt.Printf("failed to get body: %v\n", err)
+		c.Status(http.StatusNoContent)
+		return
+	}
+
+	fmt.Printf("jsonData = %v\n", string(jsonData))
+	c.JSON(http.StatusOK, gin.H{
+		"code": 200,
+		"msg":  "success",
+	})
+}
+
 func get_post(c *gin.Context) {
 	fmt.Printf("it works\n")
 }
@@ -40,6 +55,7 @@ func main() {
 	server.LoadHTMLGlob("template/*")
 	server.Use(gin.Recovery()) // (a)
 	server.GET("/", get)
+	server.POST("/post", post)
 	server.GET("/panic", func(c *gin.Context) {
 		defer func() {
 			if err := recover(); err != nil { // because of (a), return 200
@@ -48,7 +64,9 @@ func main() {
 		}()
 		panic("panic")
 	})
+
 	server.Any("/any", get_post)
+	server.Static("/wtf", "template/") // http://localhost:8888/wtf/index.html
 
 	server.Run("127.0.0.1:8888")
 }
