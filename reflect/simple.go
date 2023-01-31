d1ff119cb13ee1983862d021f3894990cfd8d58c
raybai <raybai@synology.com>
2023-01-31 15:02:01 +0800

    update reflect/

--- reflect/simple.go
+++ reflect/simple.go
@@ -6,20 +6,31 @@ import (
 )
 
 type foo struct {
-	a int
-	b float64
-	c string
-	d []int
+	A int
+	B float64
+	C string
+	D []int
 }
 
 func main() {
-	f := foo{a:1, c:"str", d:[]int{1,2,3}}
+	f := foo{A:1, C:"str", D:[]int{1,2,3}}
 	fields := reflect.TypeOf(&f).Elem()
 	vals := reflect.ValueOf(&f).Elem()
 
 	for i := 0; i < vals.NumField(); i++ {
 		field := fields.Field(i)
-		fmt.Printf("filed: %v\n", field)
+
+		if field.Name == "A" {
+			vals.Field(i).SetInt(2)
+		}
+
+		if field.Name == "D" {
+			v := vals.Field(i)
+			v = reflect.Append(v, reflect.ValueOf(4))
+			vals.Field(i).Set(v)
+		}
+
+		fmt.Printf("field: %v\n", field)
 		fmt.Printf("filed.Nmae: %v\n", field.Name)
 		fmt.Printf("vals.Field(i) = %v\n", vals.Field(i))
 	}
